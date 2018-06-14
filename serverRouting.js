0e918672f14e2f776e88efab7ec2d41222a05a88
ServerWasdabyx <michael-frevert@gmx.de>
2018-06-14 08:30:19 +0200

    Routing implemented

--- serverRouting.js
+++ serverRouting.js
@@ -1,10 +1,14 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
-	res.send('GET request to the homepage');
+app.use(express.json());
+
+app.get('/config', function(req,res){
+	res.send('GET request to the homepage\n');
 })
 
 app.put('/', function(req,res){
-	res.send('PUT request to the homepage);
+	res.send(JSON.stringify(req.body));
 });
+app.listen(8080);
