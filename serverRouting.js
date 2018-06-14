3dd398a22264a90c34aafcaf11afc5095757960a
ServerWasdabyx <michael-frevert@gmx.de>
2018-06-14 12:35:49 +0200

    changedRouting

--- serverRouting.js
+++ serverRouting.js
@@ -1,14 +1,51 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var NodeCouchDb = require('node-couchdb');
+var couch = new NodeCouchDb({
+	host:'www.wasdabyx.de',
+	protocol:'http',
+	port:5984
+});
+var couch2 = new NodeCouchDb({
+	host:'www.wasdabyx.de',
+	protocol:'http',
+	port:5984
+});
 
 app.use(express.json());
 
 app.get('/config', function(req,res){
-	res.send('GET request to the homepage\n');
-})
-
-app.put('/', function(req,res){
-	res.send(JSON.stringify(req.body));
+	couch.get("example_sensor", "config").then(({data,headers, status}) => {
+		res.send(data);
+	}, err => {
+		res.send(err.message);
+	});
 });
-app.listen(8080);
+
+var message = ' ';
+
+app.put('/', function(req, res){
+	if(req.body.id == null) {
+		res.status(404).send("Missing id");
+	}else{
+		couch.get("allraspberrys", req.body.id).then(({data, headers, status}) => {
+			res.sendStatus(200);
+			req.body.data.forEach((data) => {
+				couch2.insert("rasp_"+req.body.id, {
+					timestamp: data.timestamp,
+					humidity: data.humidity,
+					pm10: data.pm10,
+					pm25: data.pm25
+				}).then(({data, headers, status}) =>{
+					
+				}, err => {
+					
+				})
+			});
+		}, err => {
+			res.status(404).send(err.EDOCMISSING);
+		});
+	}
+});
+app.listen(65000);
