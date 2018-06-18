51c62f0630544000a34dbe97e6814817e9e64bc7
Michael Frevert <michael-frevert@gmx.de>
2018-06-18 14:39:02 +0200

    Update NodeJsTutorial.js

--- NodeJsTutorial.js
+++ NodeJsTutorial.js
@@ -1,24 +1,26 @@
 var express = require('express');
+var formidable = require('formidable');
 
 var app = express();
 
-app.use(express.static('puplic'));
-
-app.get('/index.htm', function(req, res){
-	res.sendFile(__dirname + '/' + "index.html");
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/process_get', function(req,res){
-	response = {
-		first_name:req.query.first_name,
-		last_name:req.query.last_name
-	};
-	console.log(response);
-	res.end(JSON.stringify(response));
+app.post('/', function(req,res) {
+	var form = new formidable.IncomingForm();
+	
+	form.parse(req);
+	
+	form.on('fileBegin', function(name, file) {
+		file.path = __dirname + '/uploads/' + file.name;
+	});
+	
+	form.on('file', function(name, file) {
+		console.log('Uploaded ' + file.name);
+	});
+	
+	res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(80, function(){
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log("Example app listening at http://%s:%s", host,port);
-});
+app.listen(80);
