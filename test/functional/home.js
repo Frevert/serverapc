5342d968954e03fbd9e0953f7c27fac7a490217b
Michael Frevert <michael-frevert@gmx.de>
2018-06-22 12:23:06 +0200

    Create home.js

---
+++ test/functional/home.js
@@ -0,0 +1,10 @@
+/// <reference path="../../typings/mocha/mocha.d.ts"/>
+/// <reference path="../../typings/node/node.d.ts"/>
+process.env.NODE_ENV = 'test';
+
+describe('Home page', function(){
+  it('should load the page properly');
+  it('should navigate to login');
+  it('should navigate to sign up');
+  it('should load analytics');
+});
