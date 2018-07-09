f1ac837a535483c01362bbf0f7c6e759257923fc
Michael Frevert <michael-frevert@gmx.de>
2018-07-09 10:40:29 +0200

    Update apiDataSoft.js

--- apiDataSoft.js
+++ apiDataSoft.js
@@ -1,7 +1,13 @@
 const https = require('https');
 var pm25Value='';
 var pm10Value='';
+const NodeCouchDb = require('node-couchdb');
  
+var couch = new NodeCouchDb({
+	host:'www.wasdabyx.de',
+	protocol:'http',
+	port:5984
+});
  
 https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=100&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM10&refine.land=Nordrhein-Westfalen', (resp) => {
 	let data = '';
@@ -14,18 +20,22 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
 		var apiData = JSON.parse(data);
 		var myArray = [];
 		pm10Value = apiData;
-		console.log(apiData.records.length);
 		if(pm25Value != ''){
 			for(var i = 0;i<pm10Value.records.length;i++){
 				for(var j=0; j<pm25Value.records.length; j++){
 					if(pm25Value.records[j].fields.timestamp == pm10Value.records[i].fields.timestamp && pm25Value.records[j].fields.location[0] == pm10Value.records[i].fields.location[0] && pm25Value.records[j].fields.location[1] == pm10Value.records[i].fields.location[1]){
-						var JSONString = '{"pm25": ' + pm25Value.records[j].fields.value + ', "pm10": ' + pm10Value.records[i].fields.value + ', "timestamp": ' + pm25Value.records[j].fields.timestamp + ', "lat": ' + pm25Value.records[j].fields.location[0] + ', "long": ' + pm25Value.records[j].fields.location[1] + '}';
-						myArray.push(JSONString);
-						console.log(JSONString);
+						couch.insert("api_exampleapi",{
+							timestamp: pm25Value.records[j].fields.timestamp,
+							pm10: pm10Value.records[i].fields.value,
+							pm25: pm25Value.records[j].fields.value,
+							long: pm25Value.records[j].fields.location[0],
+							lat: pm25Value.records[j].fields.location[1]
+						}).then(({data, headers, status}) => {
+						}, err => {
+						});
 					}
 				}
 			}
-			console.log(myArray.length);
 		}
 	});
 }).on('error',()=>{
@@ -42,18 +52,22 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
 		var apiData = JSON.parse(data);
 		var myArray = [];
 		pm25Value = apiData;
-		console.log("pm25:" + apiData.records.length);
 		if(pm10Value != ''){
 			for(var i = 0; i<pm10Value.records.length; i++){
 				for(var j=0; j<pm25Value.records.length; j++){
 					if(pm25Value.records[j].fields.timestamp == pm10Value.records[i].fields.timestamp && pm25Value.records[j].fields.location[0] == pm10Value.records[i].fields.location[0] && pm25Value.records[j].fields.location[1] == pm10Value.records[i].fields.location[1]){
-						var JSONString = '{"pm25": ' + pm25Value.records[j].fields.value + ', "pm10": ' + pm10Value.records[i].fields.value + ', "timestamp": ' + pm25Value.records[j].fields.timestamp + ', "lat": ' + pm25Value.records[j].fields.location[0] + ', "long": ' + pm25Value.records[j].fields.location[1] + '}';
-						myArray.push(JSONString);
-						console.log(JSONString);
+						couch.insert("api_exampleapi",{
+							timestamp: pm25Value.records[j].fields.timestamp,
+							pm10: pm10Value.records[i].fields.value,
+							pm25: pm25Value.records[j].fields.value,
+							long: pm25Value.records[j].fields.location[0],
+							lat: pm25Value.records[j].fields.location[1]
+						}).then(({data, headers, status}) => {
+						}, err => {
+						});
 					}
 				}
 			}
-			console.log(myArray.length);
 		}
 	});
 }).on('error', () => {
