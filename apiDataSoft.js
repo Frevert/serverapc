f4a7fe606d3eba7ce92f2cb675684bfabc5c8e92
Michael Frevert <michael-frevert@gmx.de>
2018-07-09 09:34:30 +0200

    Update apiDataSoft.js

--- apiDataSoft.js
+++ apiDataSoft.js
@@ -1,40 +1,39 @@
 const https = require('https');
-
-function getPM10Data() {
-	https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=100&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM10&refine.land=Nordrhein-Westfalen', (resp) => {
-		let data = '';
-		
-		resp.on('data', (chunk) => {
-			data +=chunk;
-		});
-		
-		resp.on('end', () => {
-			return data;
-		});
-	}).on('error',()=>{
-			console.log(JSON.parse(data).explanation);
+var pm25Value='';
+var pm10Value='';
+ 
+ 
+https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=100&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM10&refine.land=Nordrhein-Westfalen', (resp) => {
+	let data = '';
+	
+	resp.on('data', (chunk) => {
+		data +=chunk;
 	});
-}
-
-
-function getPM25Data() {
-	https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=100&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM2.5&refine.land=Nordrhein-Westfalen', (resp) => {
-		let data = '';
-		resp.on('data', (chunk) => {
-			data += chunk;
-		});
-		
-		resp.on('end', () => {
-			return data;
-		});
-	}).on('error', () => {
+	
+	resp.on('end' () => {
+	resp.on('end', () => {
+		var apiData = JSON.parse(data);
+		pm10Value = apiData;
+		console.log(apiData.records.length);
+	});
+}).on('error',()=>{
 		console.log(JSON.parse(data).explanation);
+});
+
+https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=100&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM2.5&refine.land=Nordrhein-Westfalen', (resp) => {
+	let data = '';
+	resp.on('data', (chunk) => {
+		data += chunk;
 	});
+	
+	resp.on('end', () => {
+		var apiData = JSON.parse(data);
+		pm25Value = apiData;
+		console.log("pm25:" + apiData.records.length);
+	});
+}).on('error', () => {
+	console.log(JSON.parse(data).explanation);
+});
+if(pm25Value != '' && pm10Value != ''){
+	console.log("Bumm");
 }
-
-var pm25Data = JSON.parse(getPM25Data);
-var pm10Data = JSON.parse(getPM10Data);
-
-console.log("PM2.5: " + pm25Data.records.length);
-console.log("PM10: " + pm10Data.records.length);
-
