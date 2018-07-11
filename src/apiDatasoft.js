d209333156a255729c1e312e5e190fb08c2d512f
Michael Frevert <michael-frevert@gmx.de>
2018-07-11 19:44:10 +0200

    test

--- src/apiDatasoft.js
+++ src/apiDatasoft.js
@@ -13,9 +13,7 @@ var couch = new NodeCouchDb({
   port: 5984,
 });
 
-/* eslint-disable */
 https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=500&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM10&refine.land=Nordrhein-Westfalen', (resp) => {
-/* eslint-enable */
   let data = '';
   var datum = date.format(new Date(), 'DD.MM.YY');
   resp.on('data', (chunk) => {
@@ -28,9 +26,7 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
     if (pm25Value !== '') {
       for (var i = 0; i < pm10Value.records.length; i++) {
         for (var j = 0; j < pm25Value.records.length; j++) {
-          /* eslint-disable */
           if (pm25Value.records[j].fields.timestamp === pm10Value.records[i].fields.timestamp && pm25Value.records[j].fields.location[0] === pm10Value.records[i].fields.location[0] && pm25Value.records[j].fields.location[1] === pm10Value.records[i].fields.location[1]) {
-          /* eslint-enable */
             couch.insert('api_opendatasoft', {
               timestamp: pm25Value.records[j].fields.timestamp,
               pm10: pm10Value.records[i].fields.value,
@@ -38,25 +34,22 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
               long: pm25Value.records[j].fields.location[1],
               lat: pm25Value.records[j].fields.location[0],
             }).then(({data, headers, status}) => {
-
+            /* jshint ignore:start */
             }, ({err}) => {
+              /* jshint ignore:end */
               console.log();
             });
             counter++;
           }
         }
       }
-      /* eslint-disable */
       console.log('Succesfull! Wrote ' + counter + ' data to api_opendatasoft on ' + datum);
-      /* eslint-enable */
     }
   });
 }).on('error', () => {
   console.log('Fehler');
 });
-/* eslint-disable */
 https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=500&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM2.5&refine.land=Nordrhein-Westfalen', (resp) => {
-/* eslint-enable */
   let data = '';
   var datum = date.format(new Date(), 'DD.MM.YY');
 
@@ -71,9 +64,7 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
     if (pm10Value !== ''){
       for (var i = 0; i < pm10Value.records.length; i++){
         for (var j = 0; j < pm25Value.records.length; j++){
-          /* eslint-disable */
           if (pm25Value.records[j].fields.timestamp === pm10Value.records[i].fields.timestamp && pm25Value.records[j].fields.location[0] === pm10Value.records[i].fields.location[0] && pm25Value.records[j].fields.location[1] === pm10Value.records[i].fields.location[1]){
-          /* eslint-enable */
             couch.insert('api_opendatasoft', {
               timestamp: pm25Value.records[j].fields.timestamp,
               pm10: pm10Value.records[i].fields.value,
@@ -81,16 +72,16 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
               long: pm25Value.records[j].fields.location[1],
               lat: pm25Value.records[j].fields.location[0],
             }).then(({data, headers, status}) => {
+              /* jshint ignore:start */
             }, (err) => {
+              /* jshint ignore:end */
               console.log();
             });
             counter++;
           }
         }
       }
-      /* eslint-disable */
       console.log('Succesfull! Wrote ' + counter + ' data to api_opendatasoft ' + datum);
-      /* eslint-enable */
     }
   });
 }).on('error', () => {
