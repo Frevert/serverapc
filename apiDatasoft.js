196fefb1d67451307496a2cf26f1ef165c349f39
Michael Frevert <michael-frevert@gmx.de>
2018-07-11 14:46:04 +0200

    Updated

--- apiDatasoft.js
+++ apiDatasoft.js
@@ -28,7 +28,9 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
     if (pm25Value !== '') {
       for (var i = 0; i < pm10Value.records.length; i++) {
         for (var j = 0; j < pm25Value.records.length; j++) {
+          /* eslint-disable */
           if (pm25Value.records[j].fields.timestamp === pm10Value.records[i].fields.timestamp && pm25Value.records[j].fields.location[0] === pm10Value.records[i].fields.location[0] && pm25Value.records[j].fields.location[1] === pm10Value.records[i].fields.location[1]) {
+          /* eslint-enable */
             couch.insert('api_opendatasoft', {
               timestamp: pm25Value.records[j].fields.timestamp,
               pm10: pm10Value.records[i].fields.value,
@@ -43,11 +45,13 @@ https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-l
           }
         }
       }
+      /* eslint-disable */
       console.log('Succesfull! Wrote ' + counter + ' data to api_opendatasoft on ' + datum);
+      /* eslint-enable */
     }
   });
 }).on('error', () => {
-  console.log(resp.err.explanation);
+  console.log("Fehler");
 });
 /* eslint-disable */
 https.get('https://public.opendatasoft.com/api/records/1.0/search/?dataset=api-luftdateninfo&rows=500&sort=-timestamp&facet=timestamp&facet=land&facet=value_type&facet=sensor_manufacturer&facet=sensor_name&refine.value_type=PM2.5&refine.land=Nordrhein-Westfalen', (resp) => {
@@ -66,7 +70,9 @@ let data = '';
     if (pm10Value !== ''){
       for (var i = 0; i < pm10Value.records.length; i++){
         for (var j = 0; j < pm25Value.records.length; j++){
+          /* eslint-disable */
           if (pm25Value.records[j].fields.timestamp === pm10Value.records[i].fields.timestamp && pm25Value.records[j].fields.location[0] === pm10Value.records[i].fields.location[0] && pm25Value.records[j].fields.location[1] === pm10Value.records[i].fields.location[1]){
+          /* eslint-enable */
             couch.insert('api_opendatasoft', {
               timestamp: pm25Value.records[j].fields.timestamp,
               pm10: pm10Value.records[i].fields.value,
@@ -75,15 +81,17 @@ let data = '';
               lat: pm25Value.records[j].fields.location[0],
             }).then(({data, headers, status}) => {
             }, err => {
-              console.log(err.explanation)
+              console.log(err.explanation);
             });
             counter++;
           }
         }
       }
+      /* eslint-disable */
       console.log('Succesfull! Wrote ' + counter + ' data to api_opendatasoft ' + datum);
+      /* eslint-enable */
     }
   });
 }).on('error', () => {
-  console.log(resp.err.explanation);
+  console.log("Fehler");
 });
